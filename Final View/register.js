010b0cb1c99a5c438a5ed82dbf1c065de9b004a5
RamisaFariha <80706411+RamisaFariha@users.noreply.github.com>
2022-10-22 23:47:15 +0600

    Add files via upload

---
+++ Final View/register.js
@@ -0,0 +1,25 @@
+function validateform(){
+    var tname = document.forms["registration_form"]["teamname"].value;
+    var email = document.forms["registration_form"]["email"].value;
+    var password = document.forms["registration_form"]["password"].value;
+    var pasrepeat = document.forms["registration_form"]["repassword"].value;
+    
+    tname = tname.trim();
+    if (tname.length < 5) {
+        alert("Teamname must contian atleast 5 characters");
+        return false;
+    }
+
+   //password = password.trim();
+    if (password.length < 5) {
+        alert("Password must contian at least 5 characters");
+        return false;
+    }
+
+    //pasrepeat = pasrepeat.trim();
+    if(password != pasrepeat){
+        alert("Both passwords does not match");
+        return false;
+    }
+}
+
